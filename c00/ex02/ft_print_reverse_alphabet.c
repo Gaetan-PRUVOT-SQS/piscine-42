48b77cd8454cdce1a66e4edf27bb15526890a585
Gaetan Pruvot <sigmaquantsystems@gmail.com>
2026-08-27 12:04:00 +0200

    Affiche l alphabet a l envers
    Meme idee que l alphabet, en partant de z.

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpruvot <gpruvot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/26 10:22:16 by gpruvot           #+#    #+#             */
+/*   Updated: 2026/08/26 12:13:34 by gpruvot          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
+
+/*int main(void)
+{
+	ft_print_reverse_alphabet();
+	return(0);	
+}*/
