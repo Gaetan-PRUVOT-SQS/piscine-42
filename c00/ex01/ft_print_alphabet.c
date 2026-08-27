2cda39e85c6e663e06e6b361a30ecc36426889c2
Gaetan Pruvot <sigmaquantsystems@gmail.com>
2026-08-27 11:18:00 +0200

    Affiche l alphabet avec ft_print_alphabet
    Premier exercice pour enchainner les caracteres.

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpruvot <gpruvot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/25 20:14:01 by gpruvot           #+#    #+#             */
+/*   Updated: 2026/08/26 10:15:10 by gpruvot          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write(1, &letter, 1);
+		letter++;
+	}
+}
+
+/*int main(void)
+{
+	ft_print_alphabet();
+	return(0);
+}*/
