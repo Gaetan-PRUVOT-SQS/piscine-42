97115639bc527c13404f64e2325f6bfac12a8503
Gaetan Pruvot <sigmaquantsystems@gmail.com>
2026-08-31 09:08:00 +0200

    Affiche les combinaisons de 3 chiffres
    Les chiffres sont uniques et dans l ordre croissant.

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpruvot <gpruvot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/26 14:26:43 by gpruvot           #+#    #+#             */
+/*   Updated: 2026/08/26 15:09:27 by gpruvot          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0' - 1;
+	while (++a <= '7')
+	{
+		b = a;
+		while (++b <= '8')
+		{
+			c = b;
+			while (++c <= '9')
+			{
+				ft_putchar(a);
+				ft_putchar(b);
+				ft_putchar(c);
+				if (!(a == '7' && b == '8' && c == '9'))
+					write(1, ", ", 2);
+			}
+		}
+	}
+}
+
+/*int main(void)
+{
+	ft_print_comb();
+	return(0);
+}*/
