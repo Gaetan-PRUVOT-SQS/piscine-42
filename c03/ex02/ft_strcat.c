a2fa3effca6dfd9013bcaad8dbd7098f9f3c10f3
Gaetan Pruvot <sigmaquantsystems@gmail.com>
2026-09-02 21:48:00 +0200

    Concatene deux chaines avec ft_strcat
    On colle src a la suite de dest.

---
+++ c03/ex02/ft_strcat.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpruvot <gpruvot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/02 18:59:20 by gpruvot           #+#    #+#             */
+/*   Updated: 2026/09/02 19:17:54 by gpruvot          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a + b] = src[b];
+		b++;
+	}
+	dest[a + b] = '\0';
+	return (dest);
+}
+
+/*int	main(void)
+{
+	char	dest[1000] = "Bonne piscine ";
+	char	src[] = "BG!\n";
+
+	ft_strcat(dest, src);
+	printf("%s", dest);
+	return (0);
+}*/
