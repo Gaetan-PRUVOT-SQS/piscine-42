10001d0afe2d4a448cca510446684e09d8c2cf83
Gaetan Pruvot <sigmaquantsystems@gmail.com>
2026-09-04 12:53:00 +0200

    Reaffiche une chaine avec ft_putstr
    On reprend write caractere par caractere.

---
+++ c04/ex01/ft_putstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpruvot <gpruvot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/03 14:22:56 by gpruvot           #+#    #+#             */
+/*   Updated: 2026/09/09 10:25:32 by gpruvot          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return ;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+/*int	main(void)
+{
+	char	*str;
+
+	str = "go!\n";
+	ft_putstr(str);
+	return (0);
+}*/
