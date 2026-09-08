145ceb903c1119ebe4db9056f008ef76b673d081
Gaetan Pruvot <sigmaquantsystems@gmail.com>
2026-09-08 09:34:00 +0200

    Passe la chaine en minuscules
    Les autres caracteres restent tels quels.

---
+++ c02/ex08/ft_strlowcase.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpruvot <gpruvot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/02 17:15:04 by gpruvot           #+#    #+#             */
+/*   Updated: 2026/09/02 17:17:55 by gpruvot          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <unistd.h>
+#include <stdio.h>
+#include <string.h>*/
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+/*int	main(void)
+{
+	char	str[] = "SaluT!";
+
+	ft_strlowcase(str);
+	printf ("%s \n", str);
+	return (0);
+}*/
