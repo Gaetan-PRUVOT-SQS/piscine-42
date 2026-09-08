d188afaa8e33ce483b1453b7063429d4a38abf2f
Gaetan Pruvot <sigmaquantsystems@gmail.com>
2026-09-08 20:14:00 +0200

    Calcule une puissance en recursif
    Le cas 0 puissance 0 renvoie 1, comme demande.

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpruvot <gpruvot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/08 08:28:13 by gpruvot           #+#    #+#             */
+/*   Updated: 2026/09/08 16:03:40 by gpruvot          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb * ft_recursive_power(nb, power - 1));
+}
+
+/*int 	main(void)
+{
+	printf("%d", ft_recursive_power(5,3));
+}*/
