0f0f73467eb89ea7992a470d4c42ab7cf2b20c00
Gaetan Pruvot <sigmaquantsystems@gmail.com>
2026-09-11 11:33:00 +0200

    Affiche les parametres tries
    Tri par ordre ascii, un argument par ligne.

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpruvot <gpruvot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/09 13:21:54 by gpruvot           #+#    #+#             */
+/*   Updated: 2026/09/11 10:42:46 by gpruvot          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return ;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_sort_params(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 1;
+		while (j < argc - i)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				tmp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc > 1)
+	{
+		ft_sort_params(argc, argv);
+		while (i < argc)
+		{
+			ft_putstr(argv[i]);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return (0);
+}
