bcbee518cabcaf2dcfc65e35c6261b2ab0b0633c
Gaetan Pruvot <sigmaquantsystems@gmail.com>
2026-09-14 08:36:00 +0200

    Duplique une chaine avec ft_strdup
    On alloue une copie et on recopie la source.

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpruvot <gpruvot@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/09 11:10:46 by gpruvot           #+#    #+#             */
+/*   Updated: 2026/09/14 08:33:33 by gpruvot          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	if (dest == 0)
+		return (0);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*#include <stdio.h>
+
+int	main(void)
+{
+	char	*a;
+
+	a = ft_strdup("42lyon");
+	printf("%s\n", a);
+	free(a);
+	return (0);
+}*/
